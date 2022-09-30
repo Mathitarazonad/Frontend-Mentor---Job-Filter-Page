fa9591332e129c98dc9f31d551ddda61147bab03
Mathias Tarazona <vladitarazona@gmail.com>
2022-09-30 16:09:48 -0400

    Uploading main files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <div id='bg-header'>
+        <img src='./images/bg-header-mobile' alt='bg-img' />
+        <img src='./images/bg-header-desktop' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+//KeepCoding
