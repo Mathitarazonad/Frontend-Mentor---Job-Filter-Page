b80123e3794484860069f041b62abee2b0d51651
Mathias Tarazona <vladitarazona@gmail.com>
2022-10-03 18:45:27 -0300

    Created Job Filtering Component

--- src/components/TagFilter.jsx
+++ src/components/TagFilter.jsx
@@ -1,2 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
 
+function TagFilter ( {selectedTags} ) {
+
+  const [active, setActive] = useState(false);
+
+  return (
+    <div className={active ? 'filter-container filter-active' : 'filter-container'}>
+      { selectedTags.map( tag => {
+        return (<div className={'selected-tag-container'} id={tag} key={tag}> 
+          <h4>{tag}</h4>
+          <img src='./images/icon-remove.svg' alt='remove'/>
+        </div>)
+      }) }
+    </div>
+  );
+}
+
+export default TagFilter;
