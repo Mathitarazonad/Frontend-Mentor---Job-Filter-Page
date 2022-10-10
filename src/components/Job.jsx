eb3d8e8bb78409f64412c2bc33136ee153a84499
Mathias Tarazona <vladitarazona@gmail.com>
2022-10-10 11:36:13 -0300

    Some styles fixed, hovers and medias for desktop

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -4,31 +4,34 @@ import { VscDebugStackframeDot } from 'react-icons/vsc';
 function Job (props) {
   return(
     <div className={props.isFeatured ? 'job featured-job' : 'job'} id={props.id}>
-      <img src={props.companyLogo} alt='company-logo' className='company-logo'></img>
-      <div className='job-info'>
-        <div className='job-header'>
-          <h3 className='company-name'>{props.companyName}</h3>
-          <div className=
-          'other-tags' style={{display: props.isNew || props.isFeatured ? 'flex' : 'none'}}>
-            <h4 
-            className='new-tag' 
-            style={{display: props.isNew ? 'initial' : 'none'}}>
-              NEW!
-            </h4>
-            <h4
-              className='featured-tag' 
-              style={{display: props.isFeatured ? 'flex' : 'none'}}>
-                FEATURED
-            </h4>
+      
+      <div className='job-info-container'>
+        <img src={props.companyLogo} alt='company-logo' className='company-logo'></img>
+        <div className='job-description'>
+          <div className='job-header'>
+            <h3 className='company-name'>{props.companyName}</h3>
+            <div className=
+            'other-tags' style={{display: props.isNew || props.isFeatured ? 'flex' : 'none'}}>
+              <h4 
+              className='new-tag' 
+              style={{display: props.isNew ? 'initial' : 'none'}}>
+                NEW!
+              </h4>
+              <h4
+                className='featured-tag' 
+                style={{display: props.isFeatured ? 'flex' : 'none'}}>
+                  FEATURED
+              </h4>
+            </div>
           </div>
-        </div>
-        <h2 className='company-job'>{props.companyJob}</h2>
-        <div className='job-details'>
-          <h5>{props.post}</h5>
-          <VscDebugStackframeDot />
-          <h5>{props.workTime}</h5>
-          <VscDebugStackframeDot />
-          <h5>{props.jobLocation}</h5>
+          <h2 className='company-job'>{props.companyJob}</h2>
+          <div className='job-details'>
+            <h5>{props.post}</h5>
+            <VscDebugStackframeDot />
+            <h5>{props.workTime}</h5>
+            <VscDebugStackframeDot />
+            <h5>{props.jobLocation}</h5>
+          </div>
         </div>
       </div>
       <hr></hr>
